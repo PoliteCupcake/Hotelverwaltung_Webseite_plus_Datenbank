1f753617db6e1edfceedc9fb8ad407acb71d0756
politeCupcake <sbieg96n@gmail.com>
2022-01-17 17:30:51 +0100

    sql_users

---
+++ sql/CREATE_TABLE_users.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE `users` (
+  `usersId` int(11) NOT NULL AUTO_INCREMENT,
+  `usersAnrede` varchar(50) DEFAULT NULL,
+  `usersNachname` varchar(255) NOT NULL,
+  `usersVorname` varchar(255) NOT NULL,
+  `usersEmail` varchar(255) NOT NULL,
+  `usersPassword` varchar(255) NOT NULL,
+  `usersUid` varchar(255) NOT NULL,
+  `usersTyp` varchar(12) NOT NULL DEFAULT 'guest',
+  PRIMARY KEY (`usersId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4	
