ebaaf63549b8cbe2a1c78cecce26e7fabc55689e
politeCupcake <sbieg96n@gmail.com>
2022-01-20 03:40:07 +0100

    Update news.php

--- news.php
+++ news.php
@@ -11,7 +11,7 @@
             echo '<div class="PageContent NewsContent">';
             echo    '<h2 class="display-5">' . $news["title"] . '</h2>';
             echo    '<p>' . $news["article"] . '</p>';
-            echo    '<p>' . $news["date"] . '</p>';
+            echo    '<p><i>gepostet: </i>' . $news["date"] . '</p>';
             echo '</div>';
         }
         else{
@@ -19,7 +19,7 @@
             echo    '<div class="row">';
             echo        '<div class="col-md-4">';
             echo            '<img class="img-fluid" src="' . $news["img_path"] . '" alt="mountains">';
-            echo             '<p>' . $news["date"] . '</p>';
+            echo             '<p> <i>gepostet: </i>' . $news["date"] . '</p>';
             echo        '</div>';
             echo        '<div class="col-md-8 border-start">';
             echo            '<h2 class="display-5">' . $news["title"] . '</h2>';
@@ -31,40 +31,4 @@
     }
 
     ?>
-
-    <!--
-    <div class="PageContent NewsContent">
-        <div class="row flex-column-reverse flex-md-row">
-            <div class="col-md-8 border-end">
-                <h2 class="display-5">Wandern!</h2>
-                <p>Ein kleiner Paragraph über die tollen Wanderrouten! Es gibt viele tolle Wanderrouten in der Umgebung. Ob zu Fuß oder mit dem Rad es ist für jeden etwas dabei!</p>
-            </div>
-            <div class="col-md-4">
-                <img class="img-fluid" src="images/mountains_news.jpg" alt="mountains">
-            </div>
-        </div>
-    </div>
-    <div class="PageContent NewsContent">
-        <div class="row">
-            <div class="col-md-4">
-                <img class="img-fluid" src="images/mountains_news.jpg" alt="mountains">
-            </div>
-            <div class="col-md-8 border-start">
-                <h2 class="display-5">Wandern!</h2>
-                <p>Ein kleiner Paragraph über die tollen Wanderrouten! Es gibt viele tolle Wanderrouten in der Umgebung. Ob zu Fuß oder mit dem Rad es ist für jeden etwas dabei!</p>
-            </div>
-        </div>
-    </div>
-    <div class="PageContent NewsContent">
-        <div class="row flex-column-reverse flex-md-row">
-            <div class="col-md-8 border-end">
-                <h2 class="display-5">Wandern!</h2>
-                <p>Ein kleiner Paragraph über die tollen Wanderrouten! Es gibt viele tolle Wanderrouten in der Umgebung. Ob zu Fuß oder mit dem Rad es ist für jeden etwas dabei!</p>
-            </div>
-            <div class="col-md-4">
-                <img class="img-fluid" src="images/mountains_news.jpg" alt="mountains">
-            </div>
-        </div>
-    </div>
--->
 </div>
